d445b9c344efa7d437f2675b6854c14272ff0a5f
Sergey-Gorb <mpost39228@mail.ru>
2021-12-07 20:46:50 +0300

    Django Les1

---
+++ apples1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Apples1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apples1'
---
+++ apples1/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, reverse
+from django.http import HttpResponse
+import os
+import datetime
+
+
+# Create your views here.
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': 'time',
+        'Показать содержимое рабочей директории': 'workdir'
+    }
+
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет,
+    # возвращается просто текст
+    current_time = datetime.datetime.today()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей
+    # директории
+    path = os.getcwd()
+    dirct = os.listdir(path)
+    msg = f'Список файлов: {dirct}'
+    return HttpResponse(msg)
